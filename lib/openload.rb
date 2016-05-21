348e024d51bdbcc6e90b0fd27d5006839243c06f
Bruno Tripoloni <bruno.tripoloni@gmail.com>
2016-05-21 15:15:19 -0300

    file status

--- lib/openload.rb
+++ lib/openload.rb
@@ -21,23 +21,33 @@ class openload
   end
 
   #Get the ticket to download a file
-  def get_download_ticket(file)
+  def download_ticket(file)
     request = get_a_request("/file/dlticket?file=#{file}&login=#{@api_login}&key=#{@api_key}") if @api_key && @api_login
     request = get_a_request("/file/dlticket?file=#{file}") unless @api_key && @api_login
     data_hash = JSON.parse(request)
     OpenStruct.new(data_hash)
   end
 
-  def get_download_link(file, ticket, captcha_response)
+  # Get the download link from a ticket
+  def download_link(file, ticket, captcha_response)
     request = get_a_request("/file/dl?file=#{file}&ticket=#{ticket}&captcha_response=#{captcha_response}") if captcha_response
     request = get_a_request("/file/dl?file=#{file}&ticket=#{ticket}") unless captcha_response
     data_hash = JSON.parse(request)
     OpenStruct.new(data_hash)
   end
 
-  def get_download_link_with_ticket(file)
+  # Get the download Link without ticket
+  def download_link_with_ticket(file)
     ticket = get_download_ticket(file)
     get_download_link(file, ticket.response.ticket)
+  end
+
+  # This method return the info of a file
+  # Warning this method rertuns a json
+  def file_info(file)
+    response = get_a_request("/file/info?file=#{file}&login=#{@api_login}&key=#{@api_key}") if @api_login && @api_key
+    response = get_a_request("/file/info?file=#{file}") unless @api_login && @api_key
+    JSON.parse(response)
   end
 
   private
