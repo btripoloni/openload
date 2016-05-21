00f94f85e1b2097a61824ebd4472cbe869706fc4
Bruno Tripoloni <bruno.tripoloni@gmail.com>
2016-05-21 16:19:29 -0300

    change http parameters

--- lib/openload.rb
+++ lib/openload.rb
@@ -5,7 +5,7 @@ class openload
   @api_url = "https://api.openload.co/1"
 
   # Create a new instace using the api-login and the api-key (both are optional)
-  def initialize(login, key)
+  def initialize(login = nil, key = nil)
     @api_login = login
     @api_key   = key
   end
@@ -22,16 +22,14 @@ class openload
 
   #Get the ticket to download a file
   def download_ticket(file)
-    request = get_a_request("/file/dlticket?file=#{file}&login=#{@api_login}&key=#{@api_key}") if @api_key && @api_login
-    request = get_a_request("/file/dlticket?file=#{file}") unless @api_key && @api_login
+    request = get_a_request("/file/dlticket?file=#{file}#{login_parameter}#{key_parameter}")
     data_hash = JSON.parse(request)
     OpenStruct.new(data_hash)
   end
 
   # Get the download link from a ticket
-  def download_link(file, ticket, captcha_response)
-    request = get_a_request("/file/dl?file=#{file}&ticket=#{ticket}&captcha_response=#{captcha_response}") if captcha_response
-    request = get_a_request("/file/dl?file=#{file}&ticket=#{ticket}") unless captcha_response
+  def download_link(file, ticket, captcha_response = nil)
+    request = get_a_request("/file/dl?file=#{file}&ticket=#{ticket}#{http_parameter('captcha_response', captcha_response)}")
     data_hash = JSON.parse(request)
     OpenStruct.new(data_hash)
   end
@@ -43,16 +41,27 @@ class openload
   end
 
   # This method return the info of a file
-  # Warning this method rertuns a json
+  # Warning this method rertuns a hash
   def file_info(file)
-    response = get_a_request("/file/info?file=#{file}&login=#{@api_login}&key=#{@api_key}") if @api_login && @api_key
-    response = get_a_request("/file/info?file=#{file}") unless @api_login && @api_key
+    response = get_a_request("/file/info?file=#{file}#{login_parameter}#{key_parameter}")
     JSON.parse(response)
   end
 
   private
   def get_a_request(path)
     HTTParty.get("#{api_url}#{'/' if path[0] != '/' }#{path}").body
+  end
+
+  def http_parameter(name, value)
+    "&#{name}=#{value}" if value
+  end
+
+  def login_parameter()
+    http_parameter('login', @api_login)
+  end
+
+  def key_parameter()
+    http_parameter('key', @api_key)
   end
 
 end
